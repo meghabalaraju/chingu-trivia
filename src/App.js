2d8416a2cc7285c1032cf82ea739975ff0662552
meghabalaraju <kbmegha12@gmail.com>
2021-08-03 13:34:05 +0200

    imports via absolute path, data fetching from api via useFetch hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { CssBaseline } from "@material-ui/core";
 import { Layout } from "./components/Layout";
+import { QuestionCard } from "./components/QuestionCard";
+import useFetch from "hooks/datafetch/useFetch";
 
 // TODO : Card, Button, Options and Data fetching
 
 function App() {
+  const url = "https://johnmeade-webdev.github.io/chingu_quiz_api/trial.json";
+  const options = {
+    method: "GET",
+  };
+
+  const { loading, data, error } = useFetch(url, options);
+
+  if (loading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <>
       <CssBaseline />
-      <Layout>This is where card is going to be</Layout>
+      <Layout>
+        <QuestionCard data={data} />
+      </Layout>
     </>
   );
 }
